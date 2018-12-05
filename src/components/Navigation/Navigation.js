61eaee20bfcdf3da46d1d1cc89831666c7ce93a8
Joel Smith <joelwsmith123@gmail.com>
2018-12-04 17:50:34 -0700

    Add navigation buttons

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,10 +4,10 @@ import './../../main.scss'
 const Navigation = () => {
   return (
     <div className='Navigation'>
-      <button className='navigation-button'><i className="fas fa-heart navigation-button-icon"></i></button>
-      <button className='navigation-button'><i className="fas fa-users navigation-button-icon"></i></button>
-      <button className='navigation-button'><i className="fas fa-globe-asia navigation-button-icon"></i></button>
-      <button className='navigation-button'><i className="fas fa-motorcycle navigation-button-icon"></i></button>
+      <button className='navigation-button navigation-button-fav'><i className="fas fa-heart navigation-button-icon"></i></button>
+      <button className='navigation-button navigation-button-people'><i className="fas fa-users navigation-button-icon"></i></button>
+      <button className='navigation-button navigation-button-planets'><i className="fas fa-globe-asia navigation-button-icon"></i></button>
+      <button className='navigation-button navigation-button-vehicles'><i className="fas fa-motorcycle navigation-button-icon"></i></button>
     </div>
   )
 }
