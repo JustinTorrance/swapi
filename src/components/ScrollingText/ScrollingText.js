9c91cf92f79d9452ec810360b9a9f9121aa98f95
Justin Torrance <justin.torrance91@gmail.com>
2018-12-04 18:04:45 -0700

    Fetch film data. Render it to the DOM.

--- src/components/ScrollingText/ScrollingText.js
+++ src/components/ScrollingText/ScrollingText.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './../../main.scss';
 
-const ScrollingText = () => {
+
+
+
+const ScrollingText = ({openingCrawl}) => {
+  console.log('episode', openingCrawl.episode)
   return (
     <div className='ScrollingText'>
       <div className="fade"></div>
       <section className="star-wars">
         <div className="crawl">
           <div className="title">
-            <p>Episode IV</p>
-            <h1>A New Hope</h1>
+            <p>{openingCrawl.episode}</p>
+            <h1>{openingCrawl.title}</h1>
           </div>    
-          <p>It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.</p>      
-          <p>During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.</p>
-          <p>Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…</p>
+          <p>{openingCrawl.body}</p>
         </div>
       </section>
     </div>
