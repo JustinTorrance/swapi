ec33b6e661ad8f8a4d93585212e6dbafe570fa6a
Joel Smith <joelwsmith123@gmail.com>
2018-12-06 17:55:52 -0700

    Refactor conditional card rendering based on displayProfile state

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,32 +13,40 @@ class Card extends Component {
     this.setState({displayProfile: !this.state.displayProfile})
   }
 
+  returnProfileSideOfCard = () => {
+    return (
+      <div className='Card' onClick={() => this.handleCardClick()}>
+        <div className='card-profile'>
+          <button className='card-favorite'><i className="fas fa-heart"></i></button>
+          <img className='card-image' src='https://moviewriternyu.files.wordpress.com/2015/07/chewy-2.png' alt='wookie' />
+          <h3>{this.props.name}</h3>
+        </div>
+      </div>
+    ) 
+  }
+
+  returnInfoSideOfCard = () => {
+    return (
+      <div className='Card' onClick={() => this.handleCardClick()}>
+        <div className='card-info'>
+          <table className='card-info-table'>
+            <tbody>
+              <tr><th className='card-info-table-name'>{this.props.name}</th></tr>
+              <tr><td className='card-info-table-species'>Species: {this.props.species}</td></tr>
+              <tr><td className='card-info-table-language'>Homeworld: {this.props.homeworld}</td></tr>
+              <tr><td className='card-info-table-data'>Homeworld Population: {this.props.homeWorldPopulation}</td></tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    ) 
+  }
+
   render() {
     if(this.state.displayProfile) {
-      return (
-        <div className='Card' onClick={() => this.handleCardClick()}>
-          <div className='card-profile'>
-            <button className='card-favorite'><i className="fas fa-heart"></i></button>
-            <img className='card-image' src='https://moviewriternyu.files.wordpress.com/2015/07/chewy-2.png' alt='wookie' />
-            <h3>{this.props.name}</h3>
-          </div>
-        </div>
-      )      
-    } else {
-      return (
-        <div className='Card' onClick={() => this.handleCardClick()}>
-          <div className='card-info'>
-            <table className='card-info-table'>
-              <tbody>
-                <tr><th className='card-info-table-name'>{this.props.name}</th></tr>
-                <tr><td className='card-info-table-species'>Species: {this.props.species}</td></tr>
-                <tr><td className='card-info-table-language'>Homeworld: {this.props.homeworld}</td></tr>
-                <tr><td className='card-info-table-data'>Homeworld Population: {this.props.homeWorldPopulation}</td></tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      )      
+      return this.returnProfileSideOfCard()
+    } else {  
+      return this.returnInfoSideOfCard()
     } 
   }
 
